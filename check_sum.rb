93ed5eeaedaf3a38cdb7f22aa22c6c24df63f98a
Torey Hickman <torey@toreyhickman.com>
2013-08-11 20:19:19 -0500

    pre_modulo_calclation method added the CheckSum class

--- check_sum.rb
+++ check_sum.rb
@@ -1,6 +1,25 @@
 class CheckSum
 
-  def is_valid?(string_of_digits)
+  def self.is_valid?(string_of_digits)
+    dividend = pre_modulo_calculation(string_of_digits)
+  end
 
+  def self.pre_modulo_calculation(string)
+    integers = string_to_array_of_integers(string)
+    products = int_times_factor(integers)
+    products.reduce(&:+)
+  end
+
+  def self.string_to_array_of_integers(string)
+    string.split('').map { |char| char.to_i }
+  end
+
+  def self.int_times_factor(array)
+    array.each_with_index.map { |n, index| (n * (9 - index)) }
   end
 end
+
+
+# tests
+puts CheckSum.pre_modulo_calculation("000000000") == 0
+puts CheckSum.pre_modulo_calculation("111111111") == 45
