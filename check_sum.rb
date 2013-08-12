bc2c39059c8c2f37415dec85c33c958b3b1bdddd
Torey Hickman <torey@toreyhickman.com>
2013-08-11 21:38:17 -0500

    Completed CheckSum is_valid? method, tests passing

--- check_sum.rb
+++ check_sum.rb
@@ -2,6 +2,7 @@ class CheckSum
 
   def self.is_valid?(string_of_digits)
     dividend = pre_modulo_calculation(string_of_digits)
+    dividend % 11 == 0
   end
 
   def self.pre_modulo_calculation(string)
@@ -23,3 +24,9 @@ end
 # tests
 puts CheckSum.pre_modulo_calculation("000000000") == 0
 puts CheckSum.pre_modulo_calculation("111111111") == 45
+puts CheckSum.is_valid?("000000000") == true
+puts CheckSum.is_valid?("490867715") == true
+puts CheckSum.is_valid?("711111111") == true
+puts CheckSum.is_valid?("777777177") == true
+puts CheckSum.is_valid?("457508000") == true
+puts CheckSum.is_valid?("664371495") == false
